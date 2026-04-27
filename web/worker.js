6394d76271101498f4f4293e2b3f0db0036df6b7
feloex <106594185+feloex@users.noreply.github.com>
2026-04-27 21:47:02 +0200

    fix: prevent website freeze by implementing web worker

---
+++ web/worker.js
@@ -0,0 +1,21 @@
+importScripts("wasm_exec.js");
+
+const go = new Go();
+let wasmReady = WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
+    go.run(result.instance);
+    console.log("loaded wasm");
+});
+
+onmessage = async function(e) {
+    await wasmReady;
+    
+    if (e.data.action == "downloadKeys") {
+        const zipArray = downloadKeys(e.data.onion, e.data.privateKey, e.data.publicKey);
+        postMessage({type: "zip", zipArray: zipArray});
+        return;
+    }
+
+    console.log("generating onion for prefix:", e.data.prefix);
+    const result = generateVanityOnion(e.data.prefix);
+    postMessage({type: "keys", result: result});
+};
